2b171ab2933f3a914ade5dde23da9bbfa0cbdc66
Hironobu OKUYAMA <okuokuoku@gmail.com>
2016-06-20 01:54:53 +0900

    Add sample solution

--- XFGoogleMapSample/Droid/MyApp.cs
+++ XFGoogleMapSample/Droid/MyApp.cs
@@ -4,10 +4,10 @@ using Android.Runtime;
 
 namespace XFGoogleMapSample.Droid
 {
-    [Android.App.Application]
+    [Application]
     [MetaData("com.google.android.maps.v2.API_KEY",
               Value = "your_google_map_key")]
-    public class MyApp : Android.App.Application
+    public class MyApp : Application
     {
         public MyApp(IntPtr javaReference, JniHandleOwnership transfer)
             : base(javaReference, transfer)
--- XFGoogleMapSample/XFGoogleMapSample/MainPage.xaml.cs
+++ XFGoogleMapSample/XFGoogleMapSample/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+using System.Linq;
 
 namespace XFGoogleMapSample
 {
@@ -7,6 +9,38 @@ namespace XFGoogleMapSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // MapType
+            map.MapType = MapType.Hybrid;
+
+            // UISettings
+            map.HasZoomEnabled = false;
+            map.HasScrollEnabled = true;
+            map.IsShowingUser = false;
+
+            // Add pin
+            var pin = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Tokyo SKYTREE",
+                Address = "Sumida-ku, Tokyo, Japan",
+                Position = new Position(35.71d, 139.81d)
+            };
+            map.Pins.Add(pin);
+
+            // Pin tap event
+            pin.Clicked += (sender, args) =>
+            {
+                DisplayAlert("tapped", "InfoWindow tapped!", "close");
+            };
+
+            // Move
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(pin.Position, Distance.FromMeters(1000)));
         }
     }
 }
