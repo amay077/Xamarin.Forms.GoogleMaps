742115efd6c4c9ddefb83a3802c60e07e5a966a7
amay077 <okuokuoku@gmail.com>
2016-06-20 05:02:54 -0700

    Add sample for UWP

--- XFGoogleMapSample/UWP/Properties/AssemblyInfo.cs
+++ XFGoogleMapSample/UWP/Properties/AssemblyInfo.cs
@@ -1,16 +1,18 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Xamarin.Forms.GoogleMaps;
-using Xamarin.Forms.GoogleMaps.UWP;
-using Xamarin.Forms.Platform.UWP;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Xamarin.Forms.GoogleMaps.WindowsUniversal")]
+[assembly: AssemblyTitle("XFGoogleMapSample.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XFGoogleMapSample.UWP")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -22,8 +24,6 @@ using Xamarin.Forms.Platform.UWP;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
-[assembly: ExportRenderer(typeof(Map), typeof(MapRenderer))]
