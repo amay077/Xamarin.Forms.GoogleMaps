eb9d4bc646ea0128c4905261739e969c06a5415e
Hironobu OKUYAMA <okuokuoku@gmail.com>
2016-07-21 02:03:53 +0900

    Implement MapClicked, MapLongClicked events

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Map.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Map.cs
@@ -32,6 +32,9 @@ namespace Xamarin.Forms.GoogleMaps
         public event EventHandler<PinDragEventArgs> PinDragEnd;
         public event EventHandler<PinDragEventArgs> PinDragging;
 
+        public event EventHandler<MapClickedEventArgs> MapClicked;
+        public event EventHandler<MapLongClickedEventArgs> MapLongClicked;
+
         MapSpan _visibleRegion;
 
         public Map(MapSpan region)
@@ -190,6 +193,16 @@ namespace Xamarin.Forms.GoogleMaps
         internal void SendPinDragging(Pin pin)
         {
             PinDragging?.Invoke(this, new PinDragEventArgs(pin));
+        }
+
+        internal void SendMapClicked(Position point)
+        {
+            MapClicked?.Invoke(this, new MapClickedEventArgs(point));
+        }
+
+        internal void SendMapLongClicked(Position point)
+        {
+            MapLongClicked?.Invoke(this, new MapLongClickedEventArgs(point));
         }
     }
 }
---
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/MapLongClickedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Xamarin.Forms.GoogleMaps
+{
+    public class MapLongClickedEventArgs : EventArgs
+    {
+        public Position Point { get; }
+
+        internal MapLongClickedEventArgs(Position point)
+        {
+            this.Point = point;
+        }
+    }
+}
