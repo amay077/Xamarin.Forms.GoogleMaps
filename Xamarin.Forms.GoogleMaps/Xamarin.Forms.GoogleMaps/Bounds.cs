0697a1e962fe8e89964ba9f41318fed9c0720db7
amay077 <okuokuoku@gmail.com>
2016-08-07 13:18:29 +0900

    Add Bounds class

---
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Bounds.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Xamarin.Forms.GoogleMaps
+{
+    public class Bounds
+    {
+        public Position SouthWest { get; }
+        public Position NorthEast { get; }
+
+        public Bounds(Position southWest, Position northEast)
+        {
+            SouthWest = southWest;
+            NorthEast = northEast;
+        }
+    }
+}
+
+
