ff824f8862b60bc800476ce495595d4524a52366
amay077 <okuokuoku@gmail.com>
2016-08-24 02:37:27 +0900

    Move Pin.IconView to BitmapFactory.FromView(View view) and add BitmapDescriptorType.View enum.

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Logics/DefaultPinLogic.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Logics/DefaultPinLogic.cs
@@ -22,7 +22,6 @@ namespace Xamarin.Forms.GoogleMaps
             else if (e.PropertyName == Pin.TypeProperty.PropertyName) OnUpdateType(outerItem, nativeItem);
             else if (e.PropertyName == Pin.IconProperty.PropertyName) OnUpdateIcon(outerItem, nativeItem);
             else if (e.PropertyName == Pin.IsDraggableProperty.PropertyName) OnUpdateIsDraggable(outerItem, nativeItem);
-            else if (e.PropertyName == Pin.IconViewProperty.PropertyName) OnUpdateIconView(outerItem, nativeItem);
         }
 
         protected abstract void OnUpdateAddress(Pin outerItem, TNative nativeItem);
@@ -36,8 +35,6 @@ namespace Xamarin.Forms.GoogleMaps
         protected abstract void OnUpdateIcon(Pin outerItem, TNative nativeItem);
 
         protected abstract void OnUpdateIsDraggable(Pin outerItem, TNative nativeItem);
-
-        protected abstract void OnUpdateIconView(Pin outerItem, TNative nativeItem);
     }
 }
 
