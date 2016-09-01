4044e340a86d8a8e14358b8f291ff81f6f0802e1
amay077 <okuokuoku@gmail.com>
2016-09-01 06:31:24 -0700

    Update file version to 1.6.0.0

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.iOS/Properties/GlobalAssemblyInfo.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.iOS/Properties/GlobalAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("Xamarin.Forms.GoogleMaps")]
 [assembly: AssemblyCopyright("Copyright © amay077. 2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.5.0.3")]
-[assembly: AssemblyFileVersion("1.5.0.3")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
