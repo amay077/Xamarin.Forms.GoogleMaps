943825372faa836e18da6ae72e46340ac4bc8652
Jean Collas <jean_collas@hotmail.com>
2016-12-10 01:38:43 +0100

    Add example

---
+++ XFGoogleMapSample/XFGoogleMapSample/NotifyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFGoogleMapSample
+{
+    public class NotifyClass : INotifyPropertyChanged
+    {
+        protected void NotifyPropertyChanged(string prop) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop)); }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
