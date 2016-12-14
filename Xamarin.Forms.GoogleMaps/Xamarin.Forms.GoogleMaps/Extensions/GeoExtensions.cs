f55c3eae5fb106f415ca3cc966977ccb9375877f
Jean Collas <jean_collas@hotmail.com>
2016-12-14 03:07:59 +0100

    More geo distances calculations

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Extensions/GeoExtensions.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Extensions/GeoExtensions.cs
@@ -20,13 +20,14 @@ namespace Xamarin.Forms.GoogleMaps
 
         public static bool IsNullOrEmpty(Position ll) { if (ll == null) return true; if (ll.Latitude == 0 || ll.Longitude == 0) return true; return false; }
 
+        #region Exact distance calculations
         public static double MilesTo(this Position gp, Position gp2)
             => KmTo(gp, gp2) * GeoConstants.MetersPerKilometer / GeoConstants.MetersPerMile;
 
         public static double MetersTo(this Position gp, Position gp2)
             => KmTo(gp, gp2) * GeoConstants.MetersPerKilometer;
 
-        public static double DegreesKmTo(double lat1Degrees, double lng1Degrees, double lat2Degrees, double lng2Degrees)
+        public static double KmTo_Degrees(double lat1Degrees, double lng1Degrees, double lat2Degrees, double lng2Degrees)
             => KmTo(lat1Degrees.ToRadians(), lng1Degrees.ToRadians(), lat2Degrees.ToRadians(), lng2Degrees.ToRadians());
 
         public static double KmTo(double lat1Rad, double lng1Rad, double lat2Rad, double lng2Rad)
@@ -42,10 +43,10 @@ namespace Xamarin.Forms.GoogleMaps
         }
 
         public static double KmTo(this Position gp, Position gp2)
-            => (gp2 == null) ? double.MaxValue : DegreesKmTo(gp.Latitude, gp.Longitude, gp2.Latitude, gp2.Longitude);
+            => (gp2 == null) ? double.MaxValue : KmTo_Degrees(gp.Latitude, gp.Longitude, gp2.Latitude, gp2.Longitude);
 
         public static double KmTo(this Position gp, double lat2Degrees, double lng2Degrees)
-            => DegreesKmTo(gp.Latitude, gp.Longitude, lat2Degrees, lng2Degrees);
+            => KmTo_Degrees(gp.Latitude, gp.Longitude, lat2Degrees, lng2Degrees);
 
         public static double KmTo(this Pin pin, Pin pin2)
             => pin.Position.KmTo(pin2.Position);
@@ -59,6 +60,78 @@ namespace Xamarin.Forms.GoogleMaps
         public static double KmTo(this IPin pin, Position pos)
             => pin.PinPosition.KmTo(pos);
 
+        #endregion Exact distance calculations
+
+        #region Approximate distance calculations
+        /// <summary>
+        /// Provides an approximation of the distance between two points based on an equirectangular projection
+        /// Calculation much faster than the exact distance, to sort positions by distance for example
+        /// </summary>
+        /// <see cref="http://www.movable-type.co.uk/scripts/latlong.html"/>
+        /// <param name="lat1Rad"></param>
+        /// <param name="lng1Rad"></param>
+        /// <param name="lat2Rad"></param>
+        /// <param name="lng2Rad"></param>
+        /// <returns></returns>
+        public static double EquirectangularKmTo(double lat1Rad, double lng1Rad, double lat2Rad, double lng2Rad)
+        {
+            // http://www.movable-type.co.uk/scripts/latlong.html
+            var x = (lng2Rad - lng1Rad) * Math.Cos((lat1Rad + lat2Rad) / 2);
+            var y = (lat2Rad - lat1Rad);
+            return Math.Sqrt(x * x + y * y) * GeoConstants.EarthRadiusKm;
+        }
+
+        public static double EquirectangularKmTo_Degrees(double lat1Deg, double lng1Deg, double lat2Deg, double lng2Deg)
+        {
+            // http://www.movable-type.co.uk/scripts/latlong.html
+            var x = (lng2Deg - lng1Deg) * ToRadiansFactorD * Math.Cos((lat1Deg + lat2Deg) * ToRadiansFactorD / 2);
+            var y = (lat2Deg - lat1Deg) * ToRadiansFactorD;
+            return Math.Sqrt(x * x + y * y) * GeoConstants.EarthRadiusKm;
+        }
+
+        public static double EquirectangularKmTo(Position p1, Position p2)
+            => EquirectangularKmTo_Degrees(p1.Latitude, p1.Longitude, p2.Latitude, p2.Longitude);
+
+
+        /// <summary>
+        /// A fast distance-like value that can be used to order positions by distance.
+        /// Not compatible with <seealso cref="DistanceToOrder_Degrees(double, double, double, double)"/>
+        /// </summary>
+        /// <param name="lat1"></param>
+        /// <param name="lng1"></param>
+        /// <param name="lat2"></param>
+        /// <param name="lng2"></param>
+        /// <returns></returns>
+        public static double Distance2ToOrder(double lat1Rad, double lng1Rad, double lat2Rad, double lng2Rad)
+        {
+            var x = (lng2Rad - lng1Rad) * Math.Cos((lat1Rad + lat2Rad) / 2);
+            var y = (lat2Rad - lat1Rad);
+            return x * x + y * y;
+        }
+
+        /// <summary>
+        /// Another fast distance-like value that can be used to order positions by distance.
+        /// Not compatible with <seealso cref="DistanceToOrder(double, double, double, double)"/>
+        /// </summary>
+        /// <param name="lat1"></param>
+        /// <param name="lng1"></param>
+        /// <param name="lat2"></param>
+        /// <param name="lng2"></param>
+        /// <returns></returns>
+        public static double DistanceToOrder_Degrees(double lat1Deg, double lng1Deg, double lat2Deg, double lng2Deg)
+        {
+            var x = (lng2Deg - lng1Deg) * Math.Cos((lat1Deg + lat2Deg) * ToRadiansFactorD / 2);
+            var y = (lat2Deg - lat1Deg);
+            return x * x + y * y;
+        }
+
+        public static double DistanceToOrder(Position p1, Position p2)
+            => DistanceToOrder_Degrees(p1.Latitude, p1.Longitude, p2.Latitude, p2.Longitude);
+
+        #endregion Approximate distance calculations
+
+        #region Position & Circle stories
+
         public static bool IsIn(this Position pos, ICircle c)
             => pos.KmTo(c.CircleCenter) <= c.CircleRadius.Kilometers;
         public static bool IsIn(this Position pos, Circle c)
@@ -69,8 +142,12 @@ namespace Xamarin.Forms.GoogleMaps
         public static bool Contains(this Circle c, Position pos)
             => IsIn(pos, c);
 
+        #endregion Position & Circle stories
+
         public static bool Intersects(this ISegment s, ICircle c)
             => s.Position1.IsIn(c) ^ s.Position2.IsIn(c);
+
+        #region Position translations
 
         public static Position TranslateToBearingDistance(this Position position1, double bearingRad, double distanceKm)
         {
@@ -96,6 +173,10 @@ namespace Xamarin.Forms.GoogleMaps
             return TranslateToBearingDistance(position1, bearing, distanceKm);
         }
 
+        #endregion Position translations
+
+        #region Segment tools
+
         public static double LengthKm(this ISegment s) => s.Position2.KmTo(s.Position1);
 
         public static double ToBearingRad(this ISegment s)
@@ -115,5 +196,8 @@ namespace Xamarin.Forms.GoogleMaps
             return Math.Atan2(y, x);
         }
         public static double ToBearingDeg(this ISegment s) => ToBearingRad(s).ToDegrees();
+
+        #endregion Segment tools
+
     }
 }
