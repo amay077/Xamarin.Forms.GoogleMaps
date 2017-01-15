418034a295038542bf40649a252199bf6b283cdb
amay077 <okuokuoku@gmail.com>
2017-01-15 20:09:22 +0900

    Update file version to 1.7.1.0

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.Android/Properties/GobalAssemblyInfo.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.Android/Properties/GobalAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("Xamarin.Forms.GoogleMaps")]
 [assembly: AssemblyCopyright("Copyright © amay077. 2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.7.0.8")]
-[assembly: AssemblyFileVersion("1.7.0.8")]
+[assembly: AssemblyVersion("1.7.1.0")]
+[assembly: AssemblyFileVersion("1.7.1.0")]
