bb640a37d12e6fbe987da30a1b881e1de7062103
deextercz <paukert.michal@gmail.com>
2017-01-18 17:51:09 +0100

    Geo Constants

---
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Extensions/GeoConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.GoogleMaps
+{
+    public static class GeoConstants
+    {
+        public const double EarthRadiusKm = 6371;
+        public const double EarthCircumferenceKm = EarthRadiusKm * 2 * Math.PI;
+        public const double MetersPerMile = 1609.344;
+        public const double MetersPerKilometer = 1000.0;
+    }
+}
