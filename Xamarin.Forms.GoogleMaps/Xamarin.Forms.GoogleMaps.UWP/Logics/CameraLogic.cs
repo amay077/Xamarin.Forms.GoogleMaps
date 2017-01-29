ec2005b47cf4cffcb6b30332187facfa8fe609a6
amay077 <okuokuoku@gmail.com>
2017-01-30 02:34:51 +0900

    [WIP]Implement MoveCamera on UWP

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.UWP/Logics/CameraLogic.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.UWP/Logics/CameraLogic.cs
@@ -7,6 +7,7 @@ using Windows.Devices.Geolocation;
 using Windows.UI.Xaml.Controls.Maps;
 using Xamarin.Forms.GoogleMaps.Internals;
 using Xamarin.Forms.GoogleMaps.Logics;
+using Xamarin.Forms.GoogleMaps.UWP.Extensions;
 
 namespace Xamarin.Forms.GoogleMaps.UWP.Logics
 {
@@ -33,9 +34,32 @@ namespace Xamarin.Forms.GoogleMaps.UWP.Logics
             await _nativeMap.TrySetViewBoundsAsync(boundingBox, null, animation);
         }
 
-        public override void OnMoveCameraRequest(CameraUpdateMessage m)
+        public async override void OnMoveCameraRequest(CameraUpdateMessage m)
         {
-            throw new NotImplementedException();
+            switch (m.Update.UpdateType)
+            {
+                case CameraUpdateType.LatLng:
+                    _nativeMap.Center = m.Update.Position.ToGeopoint();
+                    break;
+                case CameraUpdateType.LatLngZoom:
+                    _nativeMap.Center = m.Update.Position.ToGeopoint();
+                    await _nativeMap.TryZoomToAsync(m.Update.Zoom);
+                    break;
+                case CameraUpdateType.LatLngBounds:
+                    await _nativeMap.TrySetViewBoundsAsync(
+                        m.Update.Bounds.ToGeoboundingBox(), null, MapAnimationKind.None);
+                    break;
+                case CameraUpdateType.CameraPosition:
+                    _nativeMap.Center = m.Update.CameraPosition.Target.ToGeopoint();
+                    await _nativeMap.TryRotateAsync(m.Update.CameraPosition.Bearing);
+                    await _nativeMap.TryTiltToAsync(m.Update.CameraPosition.Tilt);
+                    await _nativeMap.TryZoomToAsync(m.Update.CameraPosition.Zoom);
+                    break;
+                default:
+                    break;
+            }
+
+            m.Callback.OnFinished();
         }
 
     }
