318585b7454dd2a45cc0eac1d9147f89902c5162
deextercz <paukert.michal@gmail.com>
2017-02-01 18:09:28 +0100

    Fixing camera in tabbed page

---
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Events/MapMoveEndedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.GoogleMaps.Events
+{
+    public class MapMoveEndedEventArgs
+    {
+        public MapSpan Region { get; set; }
+    }
+}
---
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Events/RegionEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Forms.GoogleMaps.Events
+{
+    public class RegionEventArgs : EventArgs
+    {
+        public MapSpan OldValue { get; set; }
+        public MapSpan NewValue { get; set; }
+    }
+}
