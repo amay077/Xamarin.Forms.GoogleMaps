178c05e77147570660a2f74e52d05e9c766a1c18
amay077 <okuokuoku@gmail.com>
2017-06-05 20:58:55 +0900

    Update file version to 2.0.0.3

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Properties/GlobalAssemblyInfo.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Properties/GlobalAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyProduct("Xamarin.Forms.GoogleMaps")]
 [assembly: AssemblyCopyright("Copyright © amay077. 2016 - 2017")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("2.0.0.2")]
-[assembly: AssemblyFileVersion("2.0.0.2")]
+[assembly: AssemblyVersion("2.0.0.3")]
+[assembly: AssemblyFileVersion("2.0.0.3")]
