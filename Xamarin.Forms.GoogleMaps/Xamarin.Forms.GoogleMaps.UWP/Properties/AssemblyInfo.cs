bacc084d81da2a8dd29dfaea6828b8c185598dfd
amay077 <okuokuoku@gmail.com>
2017-10-30 23:45:41 +0900

    Update file version to 2.2.0.2

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.UWP/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.UWP/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using Xamarin.Forms.Platform.UWP;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.2.0.1")]
-[assembly: AssemblyFileVersion("2.2.0.1")]
+[assembly: AssemblyVersion("2.2.0.2")]
+[assembly: AssemblyFileVersion("2.2.0.2")]
 [assembly: ComVisible(false)]
 [assembly: ExportRenderer(typeof(Map), typeof(MapRenderer))]
