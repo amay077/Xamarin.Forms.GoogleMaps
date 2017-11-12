a98787b5e42761fa604f15f978eb8d2b4733f4e0
amay077 <okuokuoku@gmail.com>
2017-11-12 20:36:04 +0900

    Fix comment

--- XFGoogleMapSample/Droid/mobile-center-post-clone.sh
+++ XFGoogleMapSample/Droid/mobile-center-post-clone.sh
@@ -4,5 +4,5 @@ echo "Replace $(SolutionDir) to .. in .csproj"
 perl -pi -e 's/\$\(SolutionDir\)/../g' ./XFGoogleMapSample.Droid.csproj
 perl -pi -e 's/\$\(SolutionDir\)/../g' ../XFGoogleMapSample/XFGoogleMapSample.csproj
 
-echo "Inject Google Maps API Keys"
+echo "Inject Google Maps Android API Key"
 perl -pi -e 's/your_google_maps_android_api_v2_api_key/$GOOGLEMAPS_ANDROID_API_KEY/g' ../XFGoogleMapSample/Variables_sample.cs
