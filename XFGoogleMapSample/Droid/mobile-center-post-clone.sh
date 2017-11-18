0813cec6612b4a1e06968cb147db463bec69d822
amay077 <okuokuoku@gmail.com>
2017-11-19 01:19:01 +0900

    Change single to double cout

--- XFGoogleMapSample/Droid/mobile-center-post-clone.sh
+++ XFGoogleMapSample/Droid/mobile-center-post-clone.sh
@@ -5,8 +5,4 @@ perl -pi -e 's/\$\(SolutionDir\)/../g' ./XFGoogleMapSample.Droid.csproj
 perl -pi -e 's/\$\(SolutionDir\)/../g' ../XFGoogleMapSample/XFGoogleMapSample.csproj
 
 echo "Inject Google Maps Android API Key"
-echo $GOOGLEMAPS_ANDROID_API_KEY
 perl -pi -e "s/your_google_maps_android_api_v2_api_key/$GOOGLEMAPS_ANDROID_API_KEY/g" ../XFGoogleMapSample/Variables_sample.cs
-echo perl -pi -e "s/your_google_maps_android_api_v2_api_key/$GOOGLEMAPS_ANDROID_API_KEY/g" ../XFGoogleMapSample/Variables_sample.cs
-
-cat ../XFGoogleMapSample/Variables_sample.cs
