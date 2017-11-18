413dc80c4106bea78653fadf142264bef1e4b627
amay077 <okuokuoku@gmail.com>
2017-11-19 01:04:02 +0900

    double cout

--- XFGoogleMapSample/Droid/mobile-center-post-clone.sh
+++ XFGoogleMapSample/Droid/mobile-center-post-clone.sh
@@ -6,7 +6,7 @@ perl -pi -e 's/\$\(SolutionDir\)/../g' ../XFGoogleMapSample/XFGoogleMapSample.cs
 
 echo "Inject Google Maps Android API Key"
 echo $GOOGLEMAPS_ANDROID_API_KEY
-echo perl -pi -e 's|your_google_maps_android_api_v2_api_key|$GOOGLEMAPS_ANDROID_API_KEY|g' ../XFGoogleMapSample/Variables_sample.cs
-perl -pi -e 's|your_google_maps_android_api_v2_api_key|AIzaSyDqEXkWLLLnhCTRQJmQto9e3d--HxA3DX8|g' ../XFGoogleMapSample/Variables_sample.cs
+echo perl -pi -e "s|your_google_maps_android_api_v2_api_key|$GOOGLEMAPS_ANDROID_API_KEY|g" ../XFGoogleMapSample/Variables_sample.cs
+perl -pi -e "s|your_google_maps_android_api_v2_api_key|AIzaSyDqEXkWLLLnhCTRQJmQto9e3d--HxA3DX8|g" ../XFGoogleMapSample/Variables_sample.cs
 
 cat ../XFGoogleMapSample/Variables_sample.cs
