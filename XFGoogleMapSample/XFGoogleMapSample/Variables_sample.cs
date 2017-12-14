46c3ffa91195b61fa1f556d4ac32954b440a3d9c
amay077 <okuokuoku@gmail.com>
2017-12-15 00:20:34 +0900

    update

--- XFGoogleMapSample/XFGoogleMapSample/Variables_sample.cs
+++ XFGoogleMapSample/XFGoogleMapSample/Variables_sample.cs
@@ -8,7 +8,7 @@ namespace XFGoogleMapSample
         public const string GOOGLE_MAPS_ANDROID_API_KEY = "your_google_maps_android_api_v2_api_key";
 
         // https://developers.google.com/maps/documentation/ios-sdk/start#step_4_get_an_api_key
-        public const string GOOGLE_MAPS_IOS_API_KEY = "AIzaSyC3qhk6XyYNIyS98xOHQXqDnmZ4Do6eemg";
+        public const string GOOGLE_MAPS_IOS_API_KEY = "your_google_maps_ios_sdk_api_key";
 
         // https://msdn.microsoft.com/windows/uwp/maps-and-location/authentication-key
         public const string BING_MAPS_UWP_API_KEY = "your_bing_maps_apikey";
