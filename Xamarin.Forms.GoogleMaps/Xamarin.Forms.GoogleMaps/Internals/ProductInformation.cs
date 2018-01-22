e3192ea58a6b1007c9675def480481b6ce785ace
amay077 <okuokuoku@gmail.com>
2018-01-23 00:23:46 +0900

    Update file version to 2.3.0.0

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Internals/ProductInformation.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Internals/ProductInformation.cs
@@ -5,8 +5,8 @@ namespace Xamarin.Forms.GoogleMaps.Internals
     {
         public const string Author = "amay077";
         public const string Name = "Xamarin.Forms.GoogleMaps";
-        public const string Copyright = "Copyright © amay077. 2016 - 2017";
+        public const string Copyright = "Copyright © amay077. 2016 - 2018";
         public const string Trademark = "";
-        public const string Version = "2.2.1.6";
+        public const string Version = "2.3.0.0";
     }
 }
