512e33254efb085219e913e06461363f8b7d5905
amay077 <okuokuoku@gmail.com>
2018-04-28 23:47:04 +0900

    feat: Convert csproj to .NET Standard 2.0

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Properties/AssemblyInfo.cs
@@ -8,11 +8,6 @@ using Xamarin.Forms.Internals;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Xamarin.Forms.GoogleMaps")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 [assembly: InternalsVisibleTo("Xamarin.Forms.GoogleMaps.Android")]
 [assembly: InternalsVisibleTo("Xamarin.Forms.GoogleMaps.iOS")]
 [assembly: InternalsVisibleTo("Xamarin.Forms.GoogleMaps.UWP")]
