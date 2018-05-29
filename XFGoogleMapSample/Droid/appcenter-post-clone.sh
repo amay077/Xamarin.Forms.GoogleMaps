f7d61668fe1e022f717617a82239efc519405160
amay077 <okuokuoku@gmail.com>
2018-05-30 01:22:28 +0900

    workaround: Add script for copy to Variable.cs

--- XFGoogleMapSample/Droid/appcenter-post-clone.sh
+++ XFGoogleMapSample/Droid/appcenter-post-clone.sh
@@ -4,5 +4,8 @@ echo "Replace $(SolutionDir) to .. in .csproj"
 perl -pi -e 's/\$\(SolutionDir\)/../g' ./XFGoogleMapSample.Droid.csproj
 perl -pi -e 's/\$\(SolutionDir\)/../g' ../XFGoogleMapSample/XFGoogleMapSample.csproj
 
+echo "Copy Variables_sample.cs to Variables.cs"
+cp ../XFGoogleMapSample/Variables_sample.cs ../XFGoogleMapSample/Variables.cs
+
 echo "Inject Google Maps Android API Key"
-perl -pi -e "s/your_google_maps_android_api_v2_api_key/$GOOGLEMAPS_ANDROID_API_KEY/g" ../XFGoogleMapSample/Variables_sample.cs
+perl -pi -e "s/your_google_maps_android_api_v2_api_key/$GOOGLEMAPS_ANDROID_API_KEY/g" ../XFGoogleMapSample/Variables.cs
