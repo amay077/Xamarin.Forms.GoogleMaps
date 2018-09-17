33879ad945e9c67e84ea4d90b9ba9c4b8455c401
amay077 <okuokuoku@gmail.com>
2018-09-17 15:56:44 +0900

    Update replace script2

--- XFGoogleMapSample/iOS/appcenter-post-clone.sh
+++ XFGoogleMapSample/iOS/appcenter-post-clone.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Replace $(SolutionDir) to .. in .csproj"
+perl -pi -e 's/\$\(SolutionDir\)/../g' ./XFGoogleMapSample.iOS.csproj
+perl -pi -e 's/\$\(SolutionDir\)/../g' ../XFGoogleMapSample/XFGoogleMapSample.csproj
+
+echo "Copy Variables_sample.cs to Variables.cs"
+cp ../XFGoogleMapSample/Variables_sample.cs ../XFGoogleMapSample/Variables.cs
+
 echo "Inject Google Maps API Keys"
-
-ls ./
-
 perl -pi -e "s/your_google_maps_ios_sdk_api_key/$GOOGLEMAPS_IOS_API_KEY/g" ../XFGoogleMapSample/Variables_sample.cs
