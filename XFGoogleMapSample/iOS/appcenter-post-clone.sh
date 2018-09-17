fa4765aa05132308bdb70a7a0f5a2ca7b6debe71
amay077 <okuokuoku@gmail.com>
2018-09-17 17:02:29 +0900

    test3

--- XFGoogleMapSample/iOS/appcenter-post-clone.sh
+++ XFGoogleMapSample/iOS/appcenter-post-clone.sh
@@ -8,4 +8,4 @@ echo "Copy Variables_sample.cs to Variables.cs"
 cp ../XFGoogleMapSample/Variables_sample.cs ../XFGoogleMapSample/Variables.cs
 
 echo "Inject Google Maps API Keys"
-perl -pi -e "s/your_google_maps_ios_sdk_api_key/$GOOGLEMAPS_IOS_API_KEY/g" ../XFGoogleMapSample/Variables_sample.cs
+perl -pi -e "s/your_google_maps_ios_sdk_api_key/$GOOGLEMAPS_IOS_API_KEY/g" ../XFGoogleMapSample/Variables.cs
