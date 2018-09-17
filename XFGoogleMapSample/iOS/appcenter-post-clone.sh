f2cc97540472a6e7b3db0db9706ae141a03d6716
amay077 <okuokuoku@gmail.com>
2018-09-17 18:11:52 +0900

    revert

--- XFGoogleMapSample/iOS/appcenter-post-clone.sh
+++ XFGoogleMapSample/iOS/appcenter-post-clone.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Remove _dev from bundle id in .plist"
-perl -e 's/_dev//g' ./info.plist
+perl -pi -e 's/_dev//g' ./info.plist
 
 echo "Copy Variables_sample.cs to Variables.cs"
 cp ../XFGoogleMapSample/Variables_sample.cs ../XFGoogleMapSample/Variables.cs
 
 echo "Inject Google Maps API Keys"
-perl -e "s/your_google_maps_ios_sdk_api_key/$GOOGLEMAPS_IOS_API_KEY/g" ../XFGoogleMapSample/Variables.cs
+perl -pi -e "s/your_google_maps_ios_sdk_api_key/$GOOGLEMAPS_IOS_API_KEY/g" ../XFGoogleMapSample/Variables.cs
