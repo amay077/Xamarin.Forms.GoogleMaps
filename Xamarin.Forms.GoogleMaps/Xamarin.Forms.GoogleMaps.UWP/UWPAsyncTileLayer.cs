e09da52a60d9b0d94bfee4143a287cf69429e501
James Kennedy <jameskennedy@gmpsoftware.co.uk>
2018-10-08 16:33:41 +0100

    avoid deadlocks when using custom tile layer
    avoid deadlocks when using custom tile layer

--- Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.UWP/UWPAsyncTileLayer.cs
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps.UWP/UWPAsyncTileLayer.cs
@@ -22,24 +22,31 @@ namespace Xamarin.Forms.GoogleMaps.UWP
         private async void UWPSyncTileLayer_BitmapRequested(CustomMapTileDataSource sender, MapTileBitmapRequestedEventArgs args)
         {
             var deferral = args.Request.GetDeferral();
-            var data = _makeTileUri(args.X, args.Y, args.ZoomLevel).Result;
-            if (data != null)
+            try
             {
-                MemoryStream stream = new MemoryStream();
-                stream.Write(data, 0, data.Length);
-                stream.Position = 0;
-                var decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
-                var pixelProvider = await decoder.GetPixelDataAsync(Windows.Graphics.Imaging.BitmapPixelFormat.Rgba8, Windows.Graphics.Imaging.BitmapAlphaMode.Straight, new Windows.Graphics.Imaging.BitmapTransform(), Windows.Graphics.Imaging.ExifOrientationMode.RespectExifOrientation, Windows.Graphics.Imaging.ColorManagementMode.ColorManageToSRgb);
-                var pixelData = pixelProvider.DetachPixelData();
-                InMemoryRandomAccessStream randomAccessStream = new InMemoryRandomAccessStream();
-                IOutputStream outputStream = randomAccessStream.GetOutputStreamAt(0);
-                DataWriter writer = new DataWriter(outputStream);
-                writer.WriteBytes(pixelData);
-                await writer.StoreAsync();
-                await writer.FlushAsync();
-                args.Request.PixelData = RandomAccessStreamReference.CreateFromStream(randomAccessStream);
+                var data = await _makeTileUri(args.X, args.Y, args.ZoomLevel);
+                if (data != null)
+                {
+                    MemoryStream stream = new MemoryStream();
+                    stream.Write(data, 0, data.Length);
+                    stream.Position = 0;
+                    var decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
+                    var pixelProvider = await decoder.GetPixelDataAsync(Windows.Graphics.Imaging.BitmapPixelFormat.Rgba8, Windows.Graphics.Imaging.BitmapAlphaMode.Straight, new Windows.Graphics.Imaging.BitmapTransform(), Windows.Graphics.Imaging.ExifOrientationMode.RespectExifOrientation, Windows.Graphics.Imaging.ColorManagementMode.ColorManageToSRgb);
+                    var pixelData = pixelProvider.DetachPixelData();
+                    InMemoryRandomAccessStream randomAccessStream = new InMemoryRandomAccessStream();
+                    IOutputStream outputStream = randomAccessStream.GetOutputStreamAt(0);
+                    DataWriter writer = new DataWriter(outputStream);
+                    writer.WriteBytes(pixelData);
+                    await writer.StoreAsync();
+                    await writer.FlushAsync();
+                    args.Request.PixelData = RandomAccessStreamReference.CreateFromStream(randomAccessStream);
+                }
+                deferral.Complete();
             }
-            deferral.Complete();
+            catch (Exception ex)
+            {
+                deferral.Complete();
+            }
         }
     }
 }
