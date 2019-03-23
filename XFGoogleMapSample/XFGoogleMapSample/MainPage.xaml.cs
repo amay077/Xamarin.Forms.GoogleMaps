c02c8a744d1c2409e1f5d56fa6192bf9319ca8e5
amay077 <okuokuoku@gmail.com>
2019-03-23 22:29:23 +0900

    Add sample for ItemsSource and ItemTemplate

--- XFGoogleMapSample/XFGoogleMapSample/MainPage.xaml.cs
+++ XFGoogleMapSample/XFGoogleMapSample/MainPage.xaml.cs
@@ -15,6 +15,7 @@ namespace XFGoogleMapSample
             buttonShapes2.Clicked += (_, e) => Navigation.PushAsync(new Shapes2Page());
             buttonTiles.Clicked += (_, e) => Navigation.PushAsync(new TilesPage());
             buttonCustomPins.Clicked += (_, e) => Navigation.PushAsync(new CustomPinsPage());
+            buttonPinItemsSource.Clicked += (_, e) => Navigation.PushAsync(new PinItemsSourcePage());
             buttonShapesWithInitialize.Clicked += (_, e) => Navigation.PushAsync(new ShapesWithInitializePage());
             buttonBindingPin.Clicked += (_, e) => Navigation.PushAsync(new BindingPinViewPage());
             buttonGroundOverlays.Clicked += (_, e) => Navigation.PushAsync(new GroundOverlaysPage());
