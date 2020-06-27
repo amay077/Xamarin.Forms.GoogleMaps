a9c2bc6ddca6d7a960e7ca5535534637884466cb
amay077 <okuokuoku@gmail.com>
2020-06-27 18:05:27 +0900

    Add sample

---
+++ sample/XFGoogleMapSample/Extensions/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace XFGoogleMapSample
+{
+    public static class ListExtensions
+    {
+        public static void AddAll<T>(this IList<T> self, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                self.Add(item);
+            }
+        }
+    }
+}
+
--- sample/XFGoogleMapSample/MainPage.xaml.cs
+++ sample/XFGoogleMapSample/MainPage.xaml.cs
@@ -22,12 +22,12 @@ namespace XFGoogleMapSample
             buttonPins.Clicked += (_, e) => Navigation.PushAsync(new PinsPage());
             buttonShapes.Clicked += (_, e) => Navigation.PushAsync(new ShapesPage());
             buttonShapes2.Clicked += (_, e) => Navigation.PushAsync(new Shapes2Page());
-            //buttonTiles.Clicked += (_, e) => Navigation.PushAsync(new TilesPage());
+            buttonTiles.Clicked += (_, e) => Navigation.PushAsync(new TilesPage());
             buttonCustomPins.Clicked += (_, e) => Navigation.PushAsync(new CustomPinsPage());
             buttonPinItemsSource.Clicked += (_, e) => Navigation.PushAsync(new PinItemsSourcePage());
             buttonShapesWithInitialize.Clicked += (_, e) => Navigation.PushAsync(new ShapesWithInitializePage());
-            //buttonBindingPin.Clicked += (_, e) => Navigation.PushAsync(new BindingPinViewPage());
-            //buttonGroundOverlays.Clicked += (_, e) => Navigation.PushAsync(new GroundOverlaysPage());
+            buttonBindingPin.Clicked += (_, e) => Navigation.PushAsync(new BindingPinViewPage());
+            buttonGroundOverlays.Clicked += (_, e) => Navigation.PushAsync(new GroundOverlaysPage());
             buttonMapStyles.Clicked += (_, e) => Navigation.PushAsync(new MapStylePage());
             buttonPinIconsCaching.Clicked += (_, e) => Navigation.PushAsync(new MultiplePinsIconsCaching());
 
