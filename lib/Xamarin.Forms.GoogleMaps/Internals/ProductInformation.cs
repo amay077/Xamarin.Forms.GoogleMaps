1ee6be6271766050d2408640d3ec623ceb29fc3e
amay077 <okuokuoku@gmail.com>
2020-07-26 20:05:52 +0900

    release 4.0.0-BETA1

--- lib/Xamarin.Forms.GoogleMaps/Internals/ProductInformation.cs
+++ lib/Xamarin.Forms.GoogleMaps/Internals/ProductInformation.cs
@@ -5,8 +5,8 @@ namespace Xamarin.Forms.GoogleMaps.Internals
     {
         public const string Author = "amay077";
         public const string Name = "Xamarin.Forms.GoogleMaps";
-        public const string Copyright = "Copyright © amay077. 2016 - 2019";
+        public const string Copyright = "Copyright © amay077. 2016 - 2020";
         public const string Trademark = "";
-        public const string Version = "3.3.0.0";
+        public const string Version = "3.3.0.1";
     }
 }
