07960ff2e423584271a4c8418bf4f4068aabb840
amay077 <okuokuoku@gmail.com>
2022-08-18 23:45:52 +0900

    version

---
+++ Xamarin.Forms.GoogleMaps/Xamarin.Forms.GoogleMaps/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: Preserve]
