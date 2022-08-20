e2ff10716e552b65831c8419b4c4a949d8920357
amay077 <okuokuoku@gmail.com>
2022-08-20 23:44:11 +0900

    Android sample apps

--- XFGoogleMapSample/XFGoogleMapSample.Droid/MainActivity.cs
+++ XFGoogleMapSample/XFGoogleMapSample.Droid/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Xamarin.Forms.GoogleMaps.Android;
 
 namespace XFGoogleMapSample.Droid
 {
@@ -16,6 +17,15 @@ namespace XFGoogleMapSample.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            // Override default BitmapDescriptorFactory by your implementation. 
+            var platformConfig = new PlatformConfig
+            {
+                BitmapDescriptorFactory = new CachingNativeBitmapDescriptorFactory()
+            };
+
+            Xamarin.FormsGoogleMaps.Init(this, savedInstanceState, platformConfig); // initialize for Xamarin.Forms.GoogleMaps
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
